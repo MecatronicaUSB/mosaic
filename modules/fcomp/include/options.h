ed3d83c54d62a977df130e823ffa9ab4e2ed658d
victorygc <victorygarciac@gmail.com>
2018-01-31 01:28:40 -0400

    frame rate updated, some bugs fixed

--- modules/fcomp/include/options.h
+++ modules/fcomp/include/options.h
@@ -28,11 +28,13 @@ args::Flag op_flann(op_matcher, "Flann", "Flann force matcher (default)", {'f'})
 
 args::Group op_data(sub_p, "Select imput data:", args::Group::Validators::AtMostOne);
 args::ValueFlagList<std::string> op_img(op_data, "ImageName", "Image imput name. Mus specify two file names (one per flag)",{'i'});
-args::ValueFlag<std::string> op_dir(op_data,"","Directory to load the frames. (Not yet implemented)",{'d'});
+args::ValueFlag<std::string> op_dir(op_data,"","Directory to load the frames.",{'d'});
+args::ValueFlag<std::string> op_vid(op_data,"","Video imput, frame rate is 1 fps by default. to change this options use \"-r\" command",{'v'});
 
 args::Group optional(parser, "(Optional)", args::Group::Validators::DontCare);
 args::Flag op_out(optional, "Output", "Only for images imput. Show output for good matches.", {'o'});
 args::Flag op_pre(optional, "Pre-processing", "Apply pre-processing algorithm to test improvement in keypoints search", {"pre"});
 args::Flag op_grid(optional, "grid", "Filter keypoints based on grid distribution. the grid is 10x10 cels", {"grid"});
+args::ValueFlag<int> op_frate(optional, "", "Select the desired frame rate for video input option. only for \"-v\" command", {'r',"rate"});
 
 #endif
