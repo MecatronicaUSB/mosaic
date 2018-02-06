daba2b23e04abde8e743982a59ce2497b63762ea
victorygc <victorygarciac@gmail.com>
2018-02-06 15:30:58 -0400

    Hist Stretch implemented for one channel, convert to gray scale before stretching

--- modules/fcomp/src/main.cpp
+++ modules/fcomp/src/main.cpp
@@ -161,14 +161,16 @@ int main( int argc, char** argv ) {
 
         img_ori[0] = img[0].clone();
         img_ori[1] = img[1].clone();
-        // Apply pre-processing algorithm if selected (histogram stretch)
-        if(op_pre){
-            colorChannelStretch(img[0], img[0], 1, 99);
-            colorChannelStretch(img[1], img[1], 1, 99);
-        }
+
         // Conver images to gray
         cvtColor(img[0],img[0],COLOR_BGR2GRAY);
         cvtColor(img[1],img[1],COLOR_BGR2GRAY);
+
+        // Apply pre-processing algorithm if selected (histogram stretch)
+        if(op_pre){
+            imgChannelStretch(img[0], img[0], 1, 99);
+            imgChannelStretch(img[1], img[1], 1, 99);
+        }
 
         // Detect the keypoints using desired Detector and compute the descriptors
         detector->detectAndCompute( img[0], Mat(), keypoints[0], descriptors[0] );
