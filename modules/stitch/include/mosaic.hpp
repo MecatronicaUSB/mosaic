6dc2a8aedde1efa179e372d132ba543909e50469
victorygc <victorygarciac@gmail.com>
2018-02-13 22:33:49 -0400

    Verbose update for Frame and SubMosaic Classes

--- modules/stitch/include/mosaic.hpp
+++ modules/stitch/include/mosaic.hpp
@@ -26,29 +26,48 @@
 using namespace std;
 using namespace cv;
 
-namespace m2d
+namespace m2d //!< mosaic 2d namespace
 {
-
+/**
+ * @brief 
+ */
 class SubMosaic{
     public:
-        int n_frames;
-        struct Hierarchy{
-            SubMosaic* mosaic;
-            float overlap;
-        };
-        vector<struct Hierarchy> neighbors;
-        vector<Frame*> frames;
-        Frame* key_frame;
-        Stitcher* stitcher;
-        Mat avH;
-        Mat final_scene;
+        // ---------- Atributes
+        int n_frames;                       //!< Number of frames in sub-mosaic
+        vector<Frame*> frames;              //!< Vector containing all the frames (Pointers) in sub-mosaic 
+        Frame* key_frame;                   //!< Pointer to reference frame in sub-mosaic
+        Mat final_scene;                    //!< Image containing all blended images (the sub-mosaic)
+        Mat avH;                            //!< Average Homography matrix (Matrix that reduces the dostortion error)
+        Stitcher* stitcher;                 //!< Stitcher class
+        struct Hierarchy{                   //!< Struct to relate two SubMosaics
+            SubMosaic* mosaic;              //!< Pointer to SubMosaic
+            float overlap;                  //!< Overlap area between this sub-mosaic and pointed one
+        };                  
+        vector<struct Hierarchy> neighbors; //!< Vector with all the neighbors SubMosaics (spatially close)
 
+        // ---------- Methods
+        /**
+         * @brief Default constructor
+         */
         SubMosaic() : n_frames(0){};
+        /**
+         * @brief Set the reference frame to the sub-mosaic. Create a Frame class with the input image
+         * @param _scene OpenCV Matrix containig the BGR image
+         */
         void setRerenceFrame(Mat _scene);
+        /**
+         * @brief Using the Stitcher class, add the object image to the current sub-mosaic
+         * @param _object OpenCV Matrix containig the BGR image to add in the sub-mosaic
+         * @return true If the stitch was sucesussfull
+         * @return false If the stitch wasn't sucesussfull
+         */
         bool add2Mosaic(Mat _object);
+        /**
+         * @brief Calculate the Homography matrix that reduce the distortion error in the sub-mosaic
+         * (Not yet implemented)
+         */
         void calcAverageH();
-        vector<Frame*> findNeighbors(Frame* _frame);
-
 };
 
 //TODO-----
@@ -70,7 +89,6 @@ class SubMosaic{
  * @param matches Vector with OpenCV Matches
  */
 void saveHomographyData(cv::Mat H, vector<KeyPoint> keypoints[2], vector<cv::DMatch> matches);
-
 }
 
 #endif
--- modules/stitch/include/stitch.hpp
+++ modules/stitch/include/stitch.hpp
@@ -30,7 +30,7 @@ const int TARGET_HEIGHT	= 480;
 
 namespace m2d //!< mosaic 2d namespace
 {
-/// Reference frame enumeration
+/// Reference image for stitching class
 enum ImgRef{
     SCENE,
     OBJECT
@@ -42,7 +42,7 @@ enum Detector{
     USE_SIFT,
     USE_SURF
 };
-/// Keypoint matcher to use
+/// Keypoint matcher to use in stitcher class
 enum Matcher{
     USE_BRUTE_FORCE,
     USE_FLANN
@@ -54,40 +54,67 @@ enum WarpOffset{
     LEFT,
     RIGHT
 };
-
+/// termporal frame reference for Frame class
 enum FrameRef{
     PREV,
     NEXT
 };
 
+/**
+ * @brief All data for each image in a mosaic
+ * @detail Contains All data of each image refered to current sub-mosaic or mosaic
+ */
 class Frame{
     public:
-        Mat H;
-        Rect2f bound_rect;
-        Mat color;
-        Mat gray;
-        vector<Point2f> bound_points;
-        vector<Point2f> keypoints_pos[2];
-        vector<Frame*> neighbors;
-        Frame *key_frame;
-        bool key;
+        // ---------- Atributes
+        bool key;                         //!< Flag to specify reference frame
+        Mat H;                            //!< Homography matrix based on previous frame
+        Rect2f bound_rect;                //!< Minimum bounding rectangle of transformed image
+        Mat color;                        //!< OpenCV Matrix containing the original image
+        Mat gray;                         //!< OpenCV Matrix containing a gray scale version of image
+        vector<Point2f> bound_points;     //!< Points of the transformmed image (initially at corners)
+        vector<Point2f> keypoints_pos[2]; //!< Position (X,Y) of good keypoints in image 
+        vector<Frame*> neighbors;         //!< Vector containing all spatially close Frames (Pointers)
 
+        // ---------- Methods
+        /**
+         * @brief Default class constructor
+         * @param _img OpenCV Matrix containing the BGR version of image
+         * @param _key Flag to assign this frame as reference (usefull for SubMosaic Class)
+         * @param _width Width to resize the image (Speed purpose)
+         * @param _height Height to resize the image (Speed purpose)
+         */
         Frame(Mat _img,  bool _key = false, int _width = TARGET_WIDTH, int _height = TARGET_HEIGHT);
+        /**
+         * @brief Change the reference for Homography matrix (Not yet implemented)
+         * @param _H Homography matrix
+         */
         void setHReference(Mat _H);
+        /**
+         * @brief Transform coordimates of keypoints by own homography matrix
+         * @detail Usefull to detect keypoints in original image and thack them to the transformed one
+         */
         void trackKeypoints();
+        /**
+         * @brief Check if the frame is too much distorted
+         * @detail The distortion is besed on follow criteria:
+         * - Ratio of semi-diagonals distance. \n
+         * - Area. \n
+         * - Mininum area covered by good keypoints. \n
+         * @return true if the frame is good enought 
+         * @return false otherwise
+         */
         bool isGoodFrame();
         /**
          * @brief Calculate the minimun bounding area containing good keypoints 
-         * @param keypoints Vector with OpenCV Keypoints
-         * @param matches Vector with OpenCV Matches
          * @return float Area with good keypoints inside
          */
         float boundAreaKeypoints();
         /**
          * @brief Calculate the euclidean distance between two given vector in 2D
-         * @param cv::Point2f First floating point OpenCV coordinate 
-         * @param cv::Point2f Second floating point OpenCV coordinate 
-         * @return float Distance betwenn two vector
+         * @param _pt1 First floating point OpenCV coordinate 
+         * @param _pt2 Second floating point OpenCV coordinate 
+         * @return float Distance betwenn two points
          */
         float getDistance(Point2f _pt1, Point2f _pt2);
 
@@ -97,9 +124,9 @@ class Stitcher {
     public:
         // ---------- Atributes
         Mat offset_H;
-        Mat scene_keypoints;
+        Mat scene_keypoints;                    //!< Image to draw the keypoints after one match (testing purpose)
         int cells_div;                          //!< number (n) of cell divisions in grid detector (if used)
-        vector<Frame*> img = vector<Frame*>(2); //!<     
+        vector<Frame*> img = vector<Frame*>(2); //!< Vector of two frames to stitch (Pointers)
         bool use_grid;                          //!< flag to use or not the grid detection
         bool apply_pre;                         //!< flag to apply or not SCB preprocessing algorithm
         vector<vector<cv::DMatch> > matches;    //!< Vector of OpenCV Matches                     
@@ -109,8 +136,8 @@ class Stitcher {
         /**
          * @brief Default Stitcher constructor
          * 
-         * @param _grid flag to use grid detection
-         * @param _pre flag to apply or not SCB preprocessing algorith
+         * @param _grid flag to use grid detection (default true)
+         * @param _pre flag to apply or not SCB preprocessing algorith (default false)
          * @param _width width of input images
          * @param _height width of input images
          * @param _detector enum value to set the desired feature Detector and descriptor
@@ -130,7 +157,7 @@ class Stitcher {
         void setMatcher(int);
         /**
          * @brief Set the initial scene image
-         * @param _scene OpenCV Matrix containing the initial image
+         * @param *_frame Pointer to the reference Frame
          */
         void setScene(Frame *_frame);
         /**
@@ -138,18 +165,15 @@ class Stitcher {
          * @param _object OpenCV Matrix containing the image to add to scene
          * @return bool Return true if the stitch was successful, false otherwise
          */
-        bool stitch(Frame *_object, Frame *_scene, Mat& _final_scene);
         /**
-         * @brief Measures the image distortion of next object image
-         * @detail
-         * Metrics:
-         * - Semi-diagonals ratio
-         * - Area
-         * - Minimum bounding area covered by keypoints
-         * @return bool Return true if the image is good enough, false otherwise
+         * @brief Warp and stitch the object image in the current scene
+         * @param *_object Contains the Frame to be add in the current scene
+         * @param *_scene Contains the last Frame added in the mosaic
+         * @param _final_scene OpenCV Matrix containing the final image to stitch the object
+         * @return true If the stitch was sucessfull
+         * @return false If the stitch wasn't sucessfull
          */
-        bool goodframe();
-
+        bool stitch(Frame *_object, Frame *_scene, Mat& _final_scene);
     private:
         // ---------- Atributes
         Ptr<Feature2D> detector;                //!< Pointer to OpenCV feature extractor
@@ -180,12 +204,14 @@ class Stitcher {
         void drawKeipoints();
         /**
          * @brief Computes the size of pads in the scene based on the transformation of object image
+         * @param _H Homography matrix
+         * @param _scene_dims Dimensions of scene image
          * @return vector<float> Padd size for each side of scene image
          */
         vector<float> getWarpOffet(Mat _H, Size _scene_dims);
         /**
-         * @brief Blend the warped object image to the scene
-         * @param _warp_img Input OpenCV Matrix containing warped object image
+         * @brief  Blend the warped object image to the scene
+         * @param _final_scene Input OpenCV Matrix containing warped object image
          */
         void blend2Scene(Mat &_final_scene);
         /**
